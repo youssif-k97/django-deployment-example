47146948675089acaf23822e82a06fae76c2e8d9
Youssif kmayra <youssif.kmayra97@gmail.com>
2018-05-21 19:53:37 +0200

    'first'

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+#TEMPLATE TAGGING
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('relative/', views.relative,name='relative'),
+    path('other/', views.other,name='other')
+
+]
